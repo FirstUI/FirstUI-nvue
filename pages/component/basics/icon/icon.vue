23c90c06adcd97b5136d2fe21244561e56fcb162
echo0214 <firstui@126.com>
2023-07-17 12:18:45 +0800

    nvue示例

---
+++ pages/component/basics/icon/icon.vue
@@ -0,0 +1,86 @@
+<template>
+	<view class="fui-wrap">
+		<view class="fui-page__hd">
+			<view class="fui-page__title">Icon</view>
+			<view class="fui-page__desc">Icon 图标，First UI字体图标库，可自行扩展。</view>
+		</view>
+		<view class="fui-page__bd fui-page__spacing">
+			<fui-grid :columns="3" @click="getName">
+				<fui-grid-item v-for="(item, index) in icons" :highlight="item!=''" :index="index" :key="index">
+					<view class="fui-icon__item">
+						<fui-icon :name="item"></fui-icon>
+						<text class="fui-icon__name">{{ item }}</text>
+					</view>
+				</fui-grid-item>
+			</fui-grid>
+		</view>
+	</view>
+</template>
+
+<script>
+	// #ifdef MP-BAIDU
+	import {
+		mapState
+	} from 'vuex'
+	// #endif
+	import icons from './icon.js';
+	import $fui from '@/components/firstui/fui-clipboard';
+	export default {
+		data() {
+			return {
+				icons: icons
+			}
+		},
+		// #ifdef MP-BAIDU
+		computed: mapState(['status']),
+		// #endif
+		methods: {
+			getName(e) {
+				const name = this.icons[e.detail.index]
+				if (!name) return;
+				// #ifdef MP-BAIDU
+				if (this.status == 1) {
+					$fui.getClipboardData(name, res => {
+						this.fui.toast('图标名称复制成功');
+					}, e);
+				}
+				// #endif
+
+				// #ifndef MP-BAIDU
+				$fui.getClipboardData(name, res => {
+					this.fui.toast('图标名称复制成功');
+				}, e);
+				// #endif
+			}
+		}
+	}
+</script>
+
+<style>
+	/* 头条小程序组件内不能引入字体 */
+	/* #ifdef MP-TOUTIAO */
+	@font-face {
+		font-family: 'fuiFont';
+		src: url("~@/components/firstui/fui-icon/fui-icon.ttf") format("truetype");
+	}
+
+	/* #endif */
+	page {
+		background-color: #FFFFFF;
+	}
+
+	.fui-icon__item {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+	}
+
+	.fui-icon__name {
+		font-size: 28rpx;
+		font-weight: 400;
+		padding-top: 16rpx;
+	}
+</style>
---
+++ pages/component/form/checkbox/checkbox.vue
@@ -0,0 +1,140 @@
+<template>
+	<view class="fui-wrap">
+		<view class="fui-page__hd">
+			<view class="fui-page__title">Checkbox</view>
+			<view class="fui-page__desc">Checkbox 多项选择器。</view>
+		</view>
+		<view class="fui-page__bd">
+			<view class="fui-section__title">基本使用</view>
+			<fui-checkbox-group name="checkbox" v-model="vals" @change="change">
+				<view class="fui-list__item">
+					<fui-label>
+						<view class="fui-align__center">
+							<fui-checkbox value="1"></fui-checkbox>
+							<text class="fui-text">选中</text>
+						</view>
+					</fui-label>
+					<fui-label :margin="['0','0','0','40rpx']">
+						<view class="fui-align__center">
+							<fui-checkbox value="2"></fui-checkbox>
+							<text class="fui-text">未选中</text>
+						</view>
+					</fui-label>
+				</view>
+			</fui-checkbox-group>
+			<view class="fui-section__title">推荐布局</view>
+			<fui-checkbox-group>
+				<fui-label v-for="(item,index) in checkboxItems" :key="index">
+					<fui-list-cell>
+						<view class="fui-align__center">
+							<fui-checkbox :checked="item.checked" :value="item.value" color="#FFB703"
+								borderColor="#B2B2B2">
+							</fui-checkbox>
+							<text class="fui-text">{{item.name}}</text>
+						</view>
+					</fui-list-cell>
+				</fui-label>
+			</fui-checkbox-group>
+			<view class="fui-section__title">推荐布局</view>
+			<fui-checkbox-group>
+				<fui-label v-for="(item,index) in checkboxItems" :key="index">
+					<fui-list-cell>
+						<view class="fui-list__cell">
+							<text>{{item.name}}</text>
+							<fui-checkbox :checked="item.checked" :value="item.value">
+							</fui-checkbox>
+						</view>
+					</fui-list-cell>
+				</fui-label>
+			</fui-checkbox-group>
+			<view class="fui-section__title">推荐布局</view>
+			<fui-checkbox-group>
+				<fui-label v-for="(item,index) in checkboxItems" :key="index">
+					<fui-list-cell>
+						<view class="fui-list__cell">
+							<text>{{item.name}}</text>
+							<fui-checkbox isCheckMark checkMarkColor="#FF2B2B" :scaleRatio="1.3" :checked="item.checked"
+								:value="item.value">
+							</fui-checkbox>
+						</view>
+					</fui-list-cell>
+				</fui-label>
+			</fui-checkbox-group>
+			<view class="fui-section__title">修改圆角值</view>
+			<fui-checkbox-group>
+				<fui-label v-for="(item,index) in checkboxItems" :key="index">
+					<fui-list-cell>
+						<view class="fui-align__center">
+							<fui-checkbox :checked="item.checked" :value="item.value" color="#FFB703"
+								borderColor="#B2B2B2" borderRadius="8rpx">
+							</fui-checkbox>
+							<text class="fui-text">{{item.name}}</text>
+						</view>
+					</fui-list-cell>
+				</fui-label>
+			</fui-checkbox-group>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				vals: ['1'],
+				checkboxItems: [{
+						name: '篮球',
+						value: '1',
+						checked: true
+					},
+					{
+						name: '羽毛球',
+						value: '2',
+						checked: false
+					},
+					{
+						name: '乒乓球',
+						value: '3',
+						checked: false
+					}
+				]
+			}
+		},
+		methods: {
+			change(e) {
+				console.log('change:' + JSON.stringify(e.detail.value))
+			}
+		}
+	}
+</script>
+
+<style>
+	page {
+		font-weight: normal;
+	}
+
+	.fui-section__title {
+		margin-left: 32rpx;
+	}
+
+	.fui-list__item {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		background-color: #FFFFFF;
+		padding: 28rpx 32rpx;
+		box-sizing: border-box;
+	}
+
+	.fui-text {
+		font-size: 30rpx;
+		padding-left: 16rpx;
+	}
+
+	.fui-list__cell {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+</style>
